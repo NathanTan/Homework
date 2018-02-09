114450c00cf6388ea2d3c4e3a53f14630c9d2ff6
NathanTan <tann@oregonstate.edu>
2018-02-09 09:34:15 -0800

    Range comparision is ready for use.

--- cs325/odd-coin-problem/oddCoinProblem.py
+++ cs325/odd-coin-problem/oddCoinProblem.py
@@ -1,6 +1,11 @@
 #!/bin/python3
 from random import randint
 
+greaterThan = "Greater than"
+lessThan = "Less than"
+equal = "Equal to"
+
+
 class CoinBag:
     bag = [] #smallest possible bag
     length = 0
@@ -19,14 +24,25 @@ class CoinBag:
     # This function takes in 2 tuples and signals 
     # which range of coins weights more.
     def compareRange(self, range1, range2):
+        global greaterThan
+        global lessThan
+        global equal
         low = 0
         high = 1
         weight1 = 0
         weight2 = 0
         for x in range(range1[low], range1[high]):
             weight1 = weight1 + self.bag[x].weight
+        for x in range(range2[low], range2[high]):
+            weight2 = weight2 + self.bag[x].weight
         print("Weight1: " + str(weight1))
-
+        print("Weight2: " + str(weight2))
+        if weight1 > weight2:
+            return greaterThan
+        elif weight2 > weight1:
+            return lessThan
+        else:
+            return equal
 
 class Coin:
     weight = 1.0
@@ -36,12 +52,16 @@ class Coin:
             self.weight = n
 
 def main():
+    global greaterThan
+    global lessThan
+    global equal
     print("In main")
     x = CoinBag(9)
     c = Coin()
     c2 = Coin(5.0)
     x.printBag()
-    x.compareRange((0, 9), ())
+    result = x.compareRange((0, 4), (4, 9))
+    print("Range1 is " + result + " to range2.")
 
 if __name__ == "__main__":
     main()
