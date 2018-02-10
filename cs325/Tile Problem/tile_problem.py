370d33d3971832892cd76d7afa60668a8d43bbc0
NathanTan <tann@oregonstate.edu>
2018-02-09 21:17:10 -0800

    Progress, kinda

--- cs325/Tile Problem/tile_problem.py
+++ cs325/Tile Problem/tile_problem.py
@@ -2,69 +2,98 @@
 from random import randint
 import statistics
 import math
+import sys
 
 #################### Globals ####################
 tileCount_g = 1
-
+n = -1
 
 def main():
+   global n
    print("Welcome to the tiling problem")
    width = 4
    height = 4
-   tileCount_g = 1
+   tileLevel = 1
 
+   if len(sys.argv) != 2:
+       print("Usage: python3 " + sys.argv[0] + " [Size of matrix]")
+       exit(1)
+   n = math.log(float(sys.argv[1]), 2)
+   height = int(sys.argv[1])
+   width = height
+
+   offset = 0
    graph = createGraph(width, height)
    initalizeDeadSpace(graph, width, height)
 
-   printGraph(graph, width, height)
    placeTiles(graph, width, height)
-   printGraph(graph, width, height)
+   #printGraph(graph, width, height)
 
+
+   offset = n - tileLevel
+#   placeTiles(graph, width, height, offset)
+#   printGraph(graph, width, height)
 
 
 
 
 def placeTiles(graph, width, height):
+   global n
+   global tileCount_g
+   tileLevel = 0
+   offset = 0
    quadrant = getDeadQuadrant(graph, width, height)
+   print("QUADRANT: " + str(quadrant))
    placeTile(graph, quadrant, width, height)
+   tileCount_g = tileCount_g + 1
+   printGraph(graph, width, height)
+   
+   tileLevel = 1
+   offset = n - tileLevel
+   print("Offset: " + str(offset))
+   quadrant = getDeadQuadrant(graph, width - offset, height - offset)
+   print("QUADRANT: " + str(quadrant))
+   placeTile(graph, quadrant, width - offset, height - offset)
+   tileCount_g = tileCount_g + 1
+   printGraph(graph, width, height)
 
 
 def placeTile(graph, quadrant, width, height):
    global tileCount_g
-   tileCount = tileCount_g
    xCord = math.floor(width / 2)
    yCord = math.floor(height / 2)
-   if tileCount == 1:
+   if 1 == 1:
       print("x:" + str(xCord) + " Y: " + str(yCord))
       if quadrant == 1:
          xCord = math.ceil(xCord)
          yCord = math.floor(xCord)
-         graph[xCord][yCord] = tileCount
-         graph[xCord][yCord - 1] = tileCount
-         graph[xCord - 1][yCord - 1] = tileCount
+         graph[xCord][yCord] = tileCount_g
+         graph[xCord][yCord - 1] = tileCount_g
+         graph[xCord - 1][yCord - 1] = tileCount_g
          tileCount_g = tileCount_g + 1
-         printGraph(graph, width, height)
+         printGraph(graph, int(width), int(height))
          print("Tile cnt: " + str(tileCount_g))
       elif quadrant == 2:
          #print("x: " + str(xCord) + " y: " + str(yCord))
-         graph[xCord - 1][yCord] = tileCount
-         graph[xCord][yCord - 1] = tileCount
-         graph[xCord][yCord] = tileCount
+         graph[xCord - 1][yCord] = tileCount_g
+         graph[xCord][yCord - 1] = tileCount_g
+         graph[xCord][yCord] = tileCount_g
          tileCount_g = tileCount_g + 1
          print("Tile cnt: " + str(tileCount_g))
       elif quadrant == 3:
-         graph[xCord][yCord] = tileCount
-         graph[xCord - 1][yCord] = tileCount
-         graph[xCord - 1][yCord - 1] = tileCount
+         graph[xCord][yCord] = tileCount_g
+         graph[xCord - 1][yCord] = tileCount_g
+         graph[xCord - 1][yCord - 1] = tileCount_g
       elif quadrant == 4:
-         graph[xCord][yCord - 1] = tileCount
-         graph[xCord - 1][yCord] = tileCount
-         graph[xCord - 1][yCord - 1] = tileCount
-   print("Quadrant: " + str(quadrant))
-
+         graph[xCord][yCord - 1] = tileCount_g
+         graph[xCord - 1][yCord] = tileCount_g
+         graph[xCord - 1][yCord - 1] = tileCount_g
+#   print("Quadrant: " + str(quadrant))
+   print("TILE COUNT: " + str(tileCount_g))
 
 def getDeadQuadrant(graph, width, height):
    x = -1
+
 
    # Check x quadrant
    for x in range(4):
