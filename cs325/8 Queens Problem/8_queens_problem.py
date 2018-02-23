eef05030625186a8e30081a72550e67077a2cbb9
NathanTan <tann@oregonstate.edu>
2018-02-23 13:48:36 -0800

    Added cite and solve function.

--- cs325/8 Queens Problem/8_queens_problem.py
+++ cs325/8 Queens Problem/8_queens_problem.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+# Program based off example provided here:
+# https://www.ploggingdev.com/2016/11/n-queens-solver-in-python-3/
 
 def main():
 
@@ -8,6 +10,23 @@ def main():
 
 
 
+# Solve via backtracking
+def solve(board, col, size):
+
+    # Base case
+    if col >= size:
+        return
+
+    for i in range(size):
+        if is_safe(board, i, col, size):
+            borad[i][col] = 1
+            if col == size - 1:
+                add_solution(board)
+                board[i][col] = 0
+                return
+            solve(board, col + 1, size)
+
+            board[i][col] # Backtrack step
 
 if __name__ == "__main__":
     main()
