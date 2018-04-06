69a66d02d63b2281f2bcdbe6f917bd43f244d5df
NathanTan <tann@oregonstate.edu>
2018-04-05 22:46:43 -0700

    Just playing around with Haskell.

---
+++ cs381/playground/Date.hs
@@ -0,0 +1,10 @@
+--
+-- Testing script
+--
+
+-- Variables in Haskell
+weeks :: Int
+weeks = 6
+
+days :: Int -> Int
+days w = 7 * w
---
+++ cs381/playground/HaskellExercises.hs
@@ -0,0 +1,17 @@
+-------------------------
+-- HaskellExercises.hs --
+-------------------------
+
+-- Exercise 1 --
+-- Define timeToMinutes & minutesToTime --
+--wwminutesToTime :: Int -> Int
+--minutesToTime if 1 == 1 then 
+
+
+
+
+-- Calculation Helpers --
+minutesToHours m = m / 60
+hoursToDays h = h / 24
+daysToMonths d = d / 30 -- 30 to keep it simple
+monthsToYears m = m / 12
