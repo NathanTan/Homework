0487985eb3bc5f54b3e284ed784e8a07e1cf0380
NathanTan <tann@oregonstate.edu>
2018-04-29 21:14:24 -0700

    Concurrency2 solution is now working.

--- cs444/concurrency2.c
+++ cs444/concurrency2.c
@@ -26,11 +26,14 @@ typedef struct Philosopher
 /* Fuction Headers */
 void initalizeForks();
 void *Philose(void *ph);
+void getFork(int leftFork, int rightFork, int seat);
+void putFork(int leftFork, int rightFork);
 void initalizePhilosophers(pthread_t *threads);
 
 //const char* const names[] = {"Sherk", "COnfusious", "Nicehs", "The Scientist Formally Known As Rick", "Wall-E"};
 
 sem_t forks[FORK_COUNT];
+sem_t footman;
 Philosopher philosophers[PHILOSOPHER_COUNT];
 
 void printState()
@@ -84,6 +87,7 @@ void *Philose(void *ph)
     Philosopher *p = (Philosopher *)(ph);
     int leftFork = p->seat;
     int rightFork = (p->seat + 1) % 5;
+    printf("PHIL: \n   seat: %d l: %d r: %d\n", p->seat, leftFork, rightFork);
 
     while (1)
     {
@@ -96,17 +100,34 @@ void *Philose(void *ph)
         sleep(think_time);
 
         //Wait for the forks
-        sem_wait(&forks[leftFork]);  // Wait for the fork to their left.
-        sem_wait(&forks[rightFork]); // Wait for the fork to their right.
+        getFork(leftFork, rightFork, p->seat);
 
         //eat
         printf("PHILOSOPHER: %s is going to spend %d seconds EATING\n", p->name, eat_time);
         sleep(eat_time);
 
         // Set the forks down.
-        sem_post(&forks[leftFork]);  // Set the left fork down.
-        sem_post(&forks[rightFork]); // Set the right fork down.
+        putFork(leftFork, rightFork);
     }
+}
+
+void getFork(int leftFork, int rightFork, int seat)
+{
+    sem_wait(&footman);
+    sem_wait(&forks[leftFork]); // Wait for the fork to their left.
+    printf("\t\t\tFork %d is taken by seat %d\n", leftFork, seat);
+    sem_wait(&forks[rightFork]); // Wait for the fork to their right.
+    printf("\t\t\tFork %d is taken by seat %d\n", rightFork, seat);
+}
+
+void putFork(int leftFork, int rightFork)
+{
+    sem_post(&forks[leftFork]); // Set the left fork down.
+    printf("\t\t\tFork %d is availible\n", leftFork);
+    sem_post(&forks[rightFork]); // Set the right fork down.
+    printf("\t\t\tFork %d is availible\n", rightFork);
+
+    sem_post(&footman);
 }
 
 /* A function for inializing the fork semaphores */
@@ -115,9 +136,12 @@ void initalizeForks()
     int i = 0;
     for (i = 0; i < 5; i++)
     {
-        if (sem_init(&forks[i], 0, i) == -1)
+        if (sem_init(&forks[i], 0, 1) == -1)
             printf("Error\n");
     }
+
+    if (sem_init(&footman, 0, 4) == -1)
+        printf("Error\n");
     printf("Forks initalized\n");
 }
 
@@ -126,9 +150,14 @@ void initalizePhilosophers(pthread_t *threads)
     int i = 0;
     //    for (i = 0; i < PHILOSOPHER_COUNT; i++)
     //    {
-    philosophers[i].name = "1";
+    philosophers[i].name = "p0";
     philosophers[i].activity = WAITING;
     philosophers[i].seat = i;
+    i++;
+    philosophers[i].name = "p1";
+    philosophers[i].activity = WAITING;
+    philosophers[i].seat = i;
+    //    }
     i++;
     philosophers[i].name = "p2";
     philosophers[i].activity = WAITING;
@@ -141,11 +170,6 @@ void initalizePhilosophers(pthread_t *threads)
     //    }
     i++;
     philosophers[i].name = "p4";
-    philosophers[i].activity = WAITING;
-    philosophers[i].seat = i;
-    //    }
-    i++;
-    philosophers[i].name = "p5";
     philosophers[i].activity = WAITING;
     philosophers[i].seat = i;
     //    }
